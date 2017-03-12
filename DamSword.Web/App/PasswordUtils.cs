3590879549493f43062aee7831a98494fc34a1e5
Pavel Savkin <savkin@azoft.com>
2017-03-12 17:03:12 +0700

    Implemented Password Hash generation.

---
+++ DamSword.Web/App/PasswordUtils.cs
@@ -0,0 +1,15 @@
+﻿using SimpleCrypto;
+
+namespace DamSword.Web
+{
+    public static class PasswordUtils
+    {
+        private const string Salt = "100000./zgfg8PwH08R11WoU1xMKHiLE9AVcZeQkSimax/UAdqvhw==";
+
+        public static string CreateHash(string password)
+        {
+            var cryptoService = new PBKDF2();
+            return cryptoService.Compute(password, Salt);
+        }
+    }
+}
