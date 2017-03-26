baff32deeef11565aab2d6c670502868fa16d5a5
Pavel Savkin <savkin@azoft.com>
2017-03-26 16:26:58 +0700

    Added GetOwner methods to UserRepository.

--- DamSword.Data/Repositories/UserRepository.cs
+++ DamSword.Data/Repositories/UserRepository.cs
@@ -1,9 +1,13 @@
-﻿using DamSword.Data.Entities;
+﻿using System;
+using System.Linq.Expressions;
+using DamSword.Data.Entities;
 
 namespace DamSword.Data.Repositories
 {
     public interface IUserRepository : IEntityRepository<User>
     {
+        User GetOwner();
+        TResult GetOwner<TResult>(Expression<Func<User, TResult>> selector);
     }
 
     public class UserRepository : EntityRepositoryBase<User>, IUserRepository
@@ -11,6 +15,16 @@ namespace DamSword.Data.Repositories
         public UserRepository(IEntityContext entityContext, bool asNoTracking)
             : base(entityContext, asNoTracking)
         {
+        }
+
+        public User GetOwner()
+        {
+            return GetOwner(u => u);
+        }
+
+        public TResult GetOwner<TResult>(Expression<Func<User, TResult>> selector)
+        {
+            return FirstOrDefault(u => (u.Permissions & UserPermissions.Owner) != 0, selector);
         }
     }
 }
