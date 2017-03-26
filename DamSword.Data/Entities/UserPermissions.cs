1eb595bb981763059efde6154b6ac8ea296f0884
Pavel Savkin <savkin@azoft.com>
2017-03-27 06:01:40 +0700

    Added some Permissions.

--- DamSword.Data/Entities/UserPermissions.cs
+++ DamSword.Data/Entities/UserPermissions.cs
@@ -5,6 +5,15 @@ namespace DamSword.Data.Entities
     [Flags]
     public enum UserPermissions : long
     {
+        ViewUsers = 1 << 0,
+        ManageUsers = 1 << 1,
+        ViewPersons = 1 << 10,
+        ManagePersons = 1 << 11,
+        ViewPersonOnlineMeta = 1 << 12,
+        ViewPersonDataMeta = 1 << 13,
+        ViewWatches = 1 << 20,
+        ManageWatches = 1 << 21,
+        TheQueen = 1 << 30,
         Owner = 1 << 31
     }
 }
