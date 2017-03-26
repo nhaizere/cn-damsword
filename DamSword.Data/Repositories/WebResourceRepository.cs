50fd54c8d5116420e6595930b242a21c8462d451
Pavel Savkin <savkin@azoft.com>
2017-03-27 00:38:26 +0700

    Added WebResourceRepository.

---
+++ DamSword.Data/Repositories/WebResourceRepository.cs
@@ -0,0 +1,16 @@
+﻿using DamSword.Data.Entities;
+
+namespace DamSword.Data.Repositories
+{
+    public interface IWebResourceRepository : IEntityRepository<WebResource>
+    {
+    }
+
+    public class WebResourceRepository : EntityRepositoryBase<WebResource>, IWebResourceRepository
+    {
+        public WebResourceRepository(IEntityContext entityContext, bool asNoTracking)
+            : base(entityContext, asNoTracking)
+        {
+        }
+    }
+}
