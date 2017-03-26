d2e8725e13cb05c18ddade2456a3d76bb821855e
Pavel Savkin <savkin@azoft.com>
2017-03-27 00:40:30 +0700

    Implemented FromUnixTimeStamp DateTime extension.

---
+++ DamSword.Common/App/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DamSword.Common
+{
+    public static class DateTimeExtensions
+    {
+        public static DateTime FromUnixTimeStamp(long timestamp)
+        {
+            var dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            return dateTime.AddSeconds(timestamp);
+        }
+    }
+}
