ba2dc57f90fb216a12d618d0f7bfef8229e10db4
Pavel Savkin <savkin@azoft.com>
2017-03-27 08:17:39 +0700

    Implemented CurrentUser helper class.

---
+++ DamSword.Web/App/CurrentUser.cs
@@ -0,0 +1,18 @@
+﻿using DamSword.Data.Entities;
+
+namespace DamSword.Web
+{
+    public static class CurrentUser
+    {
+        public static User User => UserScope.Current.User ?? new User
+        {
+            Alias = "Anonymous",
+            HierarcyLevel = int.MaxValue,
+            Permissions = UserPermissions.None
+        };
+
+        public static bool IsAnonymous => User.Id == 0;
+        public static long Id => User.Id;
+        public static int Hierarchy => User.HierarcyLevel;
+    }
+}
