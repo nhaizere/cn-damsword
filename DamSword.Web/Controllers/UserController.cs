04f382110128f29381cd3c21c149824b29fe7ea8
Pavel Savkin <savkin@azoft.com>
2017-03-27 15:19:02 +0700

    Implemented User List view.

---
+++ DamSword.Web/Controllers/UserController.cs
@@ -0,0 +1,49 @@
+﻿using System.Linq;
+using DamSword.Data.Entities;
+using DamSword.Data.Repositories;
+using DamSword.Web.Attributes;
+using DamSword.Web.Models.User;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DamSword.Web.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        [HttpGet, Authorize(Require = UserPermissions.ViewUsers)]
+        public IActionResult List(string search)
+        {
+            var canManage = Permissions.Has(UserPermissions.ManageUsers);
+            search = search?.ToLower() ?? string.Empty;
+
+            var users = _userRepository.Select(u => u.Id == CurrentUser.Id || u.HierarchyLevel > CurrentUser.Hierarchy,
+                u => new ListModel.User
+                {
+                    Id = u.Id,
+                    Alias = u.Alias,
+                    Login = canManage ? u.Login : null,
+                    HierarchyLevel = canManage ? u.HierarchyLevel : int.MaxValue,
+                    Permissions = canManage ? u.Permissions : UserPermissions.None
+                })
+                .Where(u => 
+                    u.Id.ToString().ToLower().Contains(search) ||
+                    u.Alias.ToLower().Contains(search) ||
+                    canManage && (u.Login.ToLower().Contains(search) || u.HierarchyLevel.ToString().ToLower().Contains(search) || u.Permissions.ToString().ToLower().Contains(search)))
+                .OrderBy(u => u.Alias)
+                .ThenBy(u => u.HierarchyLevel)
+                .ToArray();
+
+            return View("~/Views/User/List.cshtml", new ListModel
+            {
+                Search = search,
+                Users = users
+            });
+        }
+    }
+}
---
+++ DamSword.Web/Models/User/ListModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using DamSword.Data.Entities;
+
+namespace DamSword.Web.Models.User
+{
+    public class ListModel
+    {
+        public string Search { get; set; }
+        public IEnumerable<User> Users { get; set; }
+
+        public class User
+        {
+            public long Id { get; set; }
+            public string Alias { get; set; }
+            public string Login { get; set; }
+            public int HierarchyLevel { get; set; }
+            public UserPermissions Permissions { get; set; }
+        }
+    }
+}
