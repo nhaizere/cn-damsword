a9435cb90b23cd60186ab2872007a48c94f75fb0
Pavel Savkin <savkin@azoft.com>
2017-04-03 05:06:56 +0700

    Re-designed Error view.

--- DamSword.Web/Models/Error/DetailsModel.cs
+++ DamSword.Web/Models/Error/DetailsModel.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Net;
+using DamSword.Common;
 
 namespace DamSword.Web.Models.Error
 {
@@ -6,5 +8,17 @@ namespace DamSword.Web.Models.Error
     {
         public int StatusCode { get; set; }
         public Exception Exception { get; set; }
+
+        public string StatusCodeMessage
+        {
+            get
+            {
+                if (!Enum.IsDefined(typeof(HttpStatusCode), StatusCode))
+                    return "Unknown Error";
+
+                var httpStatusCode = (HttpStatusCode)StatusCode;
+                return httpStatusCode.ToString().SplitUpperCase().Join(" ");
+            }
+        }
     }
 }
