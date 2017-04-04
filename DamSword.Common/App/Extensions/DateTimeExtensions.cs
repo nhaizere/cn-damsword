74f1813eb47f06c9130d1c910529e8d6e4fb8ee1
Pavel Savkin <savkin@azoft.com>
2017-04-04 09:43:46 +0700

    Implemented ToUnixTimeStamp DateTime extension.

--- DamSword.Common/App/Extensions/DateTimeExtensions.cs
+++ DamSword.Common/App/Extensions/DateTimeExtensions.cs
@@ -4,10 +4,17 @@ namespace DamSword.Common
 {
     public static class DateTimeExtensions
     {
+        private static readonly long EpochTicks = new DateTime(1970, 1, 1).Ticks;
+
         public static DateTime FromUnixTimeStamp(long timestamp)
         {
             var dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
             return dateTime.AddSeconds(timestamp);
+        }
+
+        public static long ToUnixTimeStamp(this DateTime self)
+        {
+            return (self.Ticks - EpochTicks) / TimeSpan.TicksPerSecond;
         }
     }
 }
