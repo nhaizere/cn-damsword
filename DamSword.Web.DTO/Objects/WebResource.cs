1f906d5bf08499534c49ed3a06b18a21120103f4
Pavel Savkin <savkin@azoft.com>
2017-04-08 16:00:49 +0700

    [API] WebResource.

---
+++ DamSword.Web.DTO/Objects/WebResource.cs
@@ -0,0 +1,9 @@
+﻿namespace DamSword.Web.DTO.Objects
+{
+    public class WebResource : ObjectBase
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
---
+++ DamSword.Web/Controllers/Api/WebResouceController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq.Expressions;
+using DamSword.Data;
+using DamSword.Data.Entities;
+using DamSword.Data.Repositories;
+using DamSword.Services;
+
+namespace DamSword.Web.Controllers.Api
+{
+    public class WebResouceController : RestControllerBase<WebResource, DTO.Objects.WebResource>
+    {
+        public WebResouceController(IEntityRepository<WebResource> repository, IEntityService<WebResource> service, IUnitOfWork unitOfWork)
+            : base(repository, service, unitOfWork)
+        {
+        }
+
+        protected override UserPermissions? CreatePermissions => UserPermissions.Owner;
+        protected override UserPermissions? ReadPermissions => UserPermissions.ViewWatches;
+        protected override UserPermissions? UpdatePermissions => UserPermissions.Owner;
+        protected override UserPermissions? DeletePermissions => UserPermissions.Owner;
+
+        protected override Expression<Func<WebResource, DTO.Objects.WebResource>> ReadMap =>
+            e => new DTO.Objects.WebResource
+            {
+                Id = e.Id,
+                Name = e.Name,
+                Url = e.Url,
+                ImageUrl = e.ImageUrl
+            };
+
+        protected override Expression<Func<DTO.Objects.WebResource, WebResource>> WriteMap =>
+            o => new WebResource
+            {
+                Id = o.Id,
+                Name = o.Name,
+                Url = o.Url,
+                ImageUrl = o.ImageUrl
+            };
+    }
+}
